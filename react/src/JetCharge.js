d7749c0a24d338db3cc439850c75fcd49aeda891
John Murfett <john@jetcharge.com.au>
2021-02-11 15:58:28 +1100

    Renamed Create React App 'App' to 'JetCharge' and removed testing code

---
+++ react/src/JetCharge.js
@@ -0,0 +1,15 @@
+import './JetCharge.css';
+
+function JetCharge() {
+  return (
+    <div className="JetCharge">
+      <header className="JetCharge-header">
+        <p>
+          Include your table of data here.
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default JetCharge;
